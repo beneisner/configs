5e89064feb0df76a49d952cd695c438d44c6fb81
Ben Eisner <ben.a.eisner@gmail.com>
2024-10-05 17:31:48 -0400

    initial switch to wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,15 @@
+-- Initial.
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Fonts.
+config.font = wezterm.font("MesloLGS NF")
+config.font_size = 12
+
+-- Disable tab bar.
+config.enable_tab_bar = false
+
+-- Color
+config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+
+return config
--- .zshrc
+++ .zshrc
@@ -14,3 +14,5 @@ CONFIG_DIR=${HOME}/code/configs
 source ${CONFIG_DIR}/zsh/zshrc_base
 source ${CONFIG_DIR}/zsh/fast_paste_rc
 source ${CONFIG_DIR}/zsh/zsh_tmux
+
+# source ${CONFIG_DIR}/languages/python/python.zshrc
